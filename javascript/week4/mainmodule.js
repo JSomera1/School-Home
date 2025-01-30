4039545c1e3aa374c264b9602217923d7c98b559
Jian <jsomera1@my.bcit.ca>
2025-01-29 21:58:09 -0800

    lab 3 solution

---
+++ javascript/week4/mainmodule.js
@@ -0,0 +1,20 @@
+//function 
+const fs = require('fs') //can 
+const path = require('path')
+
+
+function mod(dirpath, extension, callback){
+    fs.readdir(dirpath, (err,list) =>{
+        if(err){
+            return callback(err)
+        }
+        const result = list.filter((file) =>{
+            return path.extname(file) === extension;
+        })
+        callback(null, result)
+
+    })
+    
+}
+
+module.exports = {mod}
--- javascript/week4/week4lab.js
+++ javascript/week4/week4lab.js
@@ -1,4 +1,19 @@
-// node app.js ..txt
-const fs = require('fs')
-const process = require('process')
-const [,,path,type] = process.argv
+const fs = require("fs")
+const path = require("path")
+const process = require("process").argv.slice(2);
+
+
+const dirpath = process[0];
+const extension = "." + process[1];
+fs.readdir(dirpath, (err, list) => {
+    if (err) {
+       return console.log(err);
+    }
+
+    const result = list.filter( (file) => {
+        return path.extname(file) === extension;
+    });
+    for (let i = 0; i < result.length; i++) {
+        console.log(result[i]);
+    }
+});
