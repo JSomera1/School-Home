cc8d21955842a5bcf4b44ae3688ce416c35454e6
Jian <jsomera1@my.bcit.ca>
2025-02-05 22:24:36 -0800

    message

---
+++ javascript/week4/adam.js
@@ -0,0 +1,60 @@
+const fs = require("node:fs");
+
+function readFileP(filename) {
+    const promObj = new Promise((resolve, reject) => {
+        fs.readFile(filename, (err, data) => {
+            if (err) {
+                reject(err); // Pending -> Rejected and Update the Promise Result
+            }
+            else {
+                resolve(data.toString()); // Pending -> Fulfilled
+            }
+        });
+    });
+    return promObj;
+};
+
+function turnIntoNiceMenu(data) {
+    const lines = data.split("\n")
+    const categories = {};
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        const [mealType, mealName, mealQuantity, price] = line.split(",");
+        const correctFormat = `$${parseFloat(price.slice(1)).toFixed(2)} ${mealName.trim()} ${mealQuantity.trim()}`
+
+        if (!categories[mealType.trim()]){
+            categories[mealType.trim()] = [];
+        }
+        categories[mealType.trim()].push(correctFormat)
+    }
+
+    completedOutput = "";
+
+    for (const category in categories) {
+        completedOutput = completedOutput + `* ${category.charAt(0).toUpperCase() + category.slice(1)} Items *\n`
+        completedOutput = completedOutput + categories[category].join("\n") + "\n\n";
+        console.log(categories)
+    }
+    return completedOutput
+}
+
+function writeFileP(filename, data) {
+    const promObj = new Promise((resolve, reject) => {
+        fs.writeFile(filename, data, (err) => {
+            if (err){
+                reject(err);
+            }
+            else {
+                resolve();
+            }
+        })
+    })
+    return promObj;
+}
+
+readFileP("menu.csv")
+    .then((csvContents) => turnIntoNiceMenu(csvContents))
+    .then((niceMenuStr) => writeFileP("nicemenu.txt", niceMenuStr))
+    .then(() => console.log("Program Complete"))
+    .catch((err) => console.log(err));
--- javascript/week4/app.js
+++ javascript/week4/app.js
@@ -12,30 +12,7 @@ function readFileN(filename){
     })
 }
 
-function nicemenu(data){
-    const reads = data.split("\n")
-    const categories = {}
-    const completedOutput = ''
-    for (let i = 0; i< reads.length;i++){
-        const line = reads[i]
-        const[MealType,MealName,MealQuantity,Price] = line.split(",")
-        const correctFormat = `$${parseFloat(Price.slice(1)).toFixed(2)}  ${MealName.trim()}, ${MealQuantity.trim()}`
 
-    
-        if(!categories[MealType.trim()]){
-            categories[MealType.trim()] = []
-        }
-        categories[MealType.trim()].push(correctFormat)
-    }
-
-    for (let j = 0;j<categories.length;j++) {
-        // completedOutput = completedOutput + `* ${categories[j].charAt(0).toUpperCase() + categories[j].slice(1)} Items *\n`
-        // completedOutput = completedOutput + categories[j].join("\n") + "\n\n";
-        console.log(categories[j])
-    }
-
-    return completedOutput
-}
 
 function writeFileP(filename, data) {
     return new Promise((resolve, reject) => {
@@ -52,7 +29,30 @@ function writeFileP(filename, data) {
 
 
 readFileN('menu.csv')
-.then((data) => nicemenu(data))
+.then((data) => {
+    const reads = data.split("\n")
+    const categories = {}
+    
+    for (let i = 0; i< reads.length;i++){
+        const line = reads[i]
+        const[MealType,MealName,MealQuantity,Price] = line.split(",")
+        const correctFormat = `$${parseFloat(Price.slice(1)*1.8).toFixed(2)}  ${MealName.trim()}, ${MealQuantity.trim()}`
+
+        if(!categories[MealType.trim()]){
+            categories[MealType.trim()] = []
+        }
+        categories[MealType.trim()].push(correctFormat)
+    }
+
+    completedOutput = ''
+
+    for (const category in categories) {
+        completedOutput = completedOutput + `* ${category.charAt(0).toUpperCase() + category.slice(1)} Items *\n`
+        completedOutput = completedOutput + categories[category].join("\n") + "\n\n";
+    }
+    
+    return completedOutput
+})
 .then((menu) => writeFileP('menu.txt', menu))
 .then(console.log("Program complete"))
 .catch((err) => console.log(err))
